7031aba6ce2bcb4c827a3d743bb6884855fe4876
Pedro Liberatti <datcrazydelphox@gmail.com>
2021-05-20 09:19:46 -0300

    use temp dir instead of removing files

--- Utilities/encode.py
+++ Utilities/encode.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-import os, sys, argparse, time, subprocess, signal
+import os, sys, argparse, time, subprocess, signal, tempfile
 import ffpb
 
 
@@ -21,24 +21,10 @@ class style():
     red = '\033[1m\033[31m'
     reset = '\033[0m'
 
-
-def remove_ffmpeg_remains():
-    try:
-        os.remove("ffmpeg2pass-0.log")
-        os.remove("ffmpeg2pass-0.log.mbtree")
-    except:
-        pass
-    try:
-        os.remove("ffmpeg2pass-0.log.temp")
-        os.remove("ffmpeg2pass-0.log.mbtree.temp")
-    except:
-        pass
-
 if __name__ == '__main__':
     os.system("") #Because Windows is stupid and doesn't like ANSI colors
     args = parser.parse_args()
     def signal_handler(signal, frame):
-        remove_ffmpeg_remains()
         sys.exit(0)
     signal.signal(signal.SIGINT, signal_handler)
 
@@ -88,13 +74,13 @@ def do_conversion():
         if args.o != None:
             print(style.red + 'Syntax Error: "-o" argument must not be used when converting entire directories' + style.reset)
             sys.exit(1)
-        file = [entry.path for entry in os.scandir(args.input) if entry.is_file()]
+        mov = [entry.path for entry in os.scandir(args.input) if entry.is_file()]
     elif os.path.isfile(args.input):
-        file = [args.input]
+        mov = [args.input]
         if args.o != None:
             print(style.yellow + 'WARNING: ' + style.white + 'Using "-o" will override the "-f" argument' + style.reset)
     
-    for filename in file:
+    for filename in mov:
         
         arg_list = ['-hide_banner', '-y', '-i', filename, '-c:v', get_encoder(), '-preset', args.p, '-b:v', calculate_bitrate(filename, args.size), '-an', '-pass', '1', '-f', 'matroska', os.devnull]
         arg_pass = ['-hide_banner', '-y', '-i', filename, '-c:v', get_encoder(), '-preset', args.p, '-b:v', calculate_bitrate(filename, args.size), '-map', '0', '-c:a', 'copy', '-pass', '2', outfile(filename)]
@@ -106,7 +92,9 @@ def do_conversion():
         print_slow((style.white + 'Expected video bitrate will be: ' + style.reset + '~' + calculate_bitrate(filename, args.size)), 0.5)
         print_slow(style.blue + 'Audio bitrate will remain as-is\n' + style.reset, 0.5)
         
-        
+        tmp = tempfile.TemporaryDirectory()
+        os.chdir(tmp.name)
+
         print_slow('Running ' + style.green + '1st' + style.reset + ' pass...', 0.5)
         ffpb.main(argv=arg_list)
 
@@ -114,7 +102,6 @@ def do_conversion():
         ffpb.main(argv=arg_pass)
         
         print('\nCleaning up...')
-        remove_ffmpeg_remains()
     time.sleep(0.5)
     print(style.green + 'Done' + style.reset)
     sys.exit(0)
